3709ab04f598990a2970eee9755b4c4c2ba3739e
jerryzhao173985 <44931279+jerryzhao173985@users.noreply.github.com>
2018-11-24 16:57:45 +0000

    Add files via upload

--- src/pages/CreateQueue.js
+++ src/pages/CreateQueue.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Router, navigate } from '@reach/router'
 
 class CreateQueue extends React.Component {
+	constructor(props){
+        super(props);
+        this.state={
+            queue : ''
+        }
+    }
+
+    _onSubmit= event=>{
+        console.log(this.state);
+        
+        event.preventDefault();
+    }
+
+    handleChange= (event)=>{ 
+        this.setState({
+            queue: event.target.value
+        });
+
+    event.preventDefault();
+    }
+
     render() {
         return (
-            <div>Create another queue page!</div>
+            <form onSubmit={this._onSubmit}>
+                 <div className="CreateQueue">  
+                    <label>
+                    	Create a queue: 
+                        <br></br>
+                    	<input type="text" value = {this.state.queue}  onChange={this.handleChange} />
+                        <br></br>
+                     </label>
+                     <input type="submit" value="Create" />
+                </div>
+            </form>
         )
     }
 }
